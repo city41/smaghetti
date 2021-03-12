74a547bde7cf24bb377a1ca8d4c462b82ab4060c
Matt Greer <matt.e.greer@gmail.com>
2021-03-12 15:17:43 -0500

    supabase: update to NEXT env variables

--- src/remoteData/client.ts
+++ src/remoteData/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const client = createClient(
-  process.env.GATSBY_SUPABASE_URL as string,
-  process.env.GATSBY_SUPABASE_KEY as string
+	process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+	process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 export { client };
