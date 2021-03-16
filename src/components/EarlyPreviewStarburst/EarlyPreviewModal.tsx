f5f3642c1b09ba365cb6844d4e00f238d1ff0f8d
Matt Greer <matt.e.greer@gmail.com>
2021-03-16 14:57:29 -0400

    EarlyPreviewModal: simpler text for now

--- src/components/EarlyPreviewStarburst/EarlyPreviewModal.tsx
+++ src/components/EarlyPreviewStarburst/EarlyPreviewModal.tsx
@@ -8,68 +8,62 @@ type EarlyPreviewModalProps = {
 	mode: 'editor' | 'player';
 };
 
-function EditorHeadsUp() {
-	return (
-		<>
-			<li>There are bugs!</li>
-			<li>
-				There aren&apos;t too many items, enemies, etc yet. Check the{' '}
-				<InternalLink to="/roadmap">roadmap</InternalLink> to see where things
-				are at
-			</li>
-			<li>
-				You can share your level with friends by clicking the share button in
-				the far upper right
-			</li>
-			<li>
-				But ... since we are actively developing the game, we can&apos;t promise
-				your level will work forever
-			</li>
-		</>
-	);
-}
-
-function PlayerHeadsUp() {
-	return (
-		<>
-			<li>There are bugs!</li>
-			<li>
-				You can edit this level by clicking the link down in the lower right
-			</li>
-			<li>
-				You can use your keyboard or a gamepad. The ability to remap the
-				controls is coming.
-			</li>
-		</>
-	);
-}
+// function EditorHeadsUp() {
+// 	return (
+// 		<>
+// 			<li>There are bugs!</li>
+// 			<li>
+// 				There aren&apos;t too many items, enemies, etc yet. Check the{' '}
+// 				<InternalLink to="/roadmap">roadmap</InternalLink> to see where things
+// 				are at
+// 			</li>
+// 			<li>
+// 				You can share your level with friends by clicking the share button in
+// 				the far upper right
+// 			</li>
+// 			<li>
+// 				But ... since we are actively developing the game, we can&apos;t promise
+// 				your level will work forever
+// 			</li>
+// 		</>
+// 	);
+// }
+//
+// function PlayerHeadsUp() {
+// 	return (
+// 		<>
+// 			<li>There are bugs!</li>
+// 			<li>
+// 				You can edit this level by clicking the link down in the lower right
+// 			</li>
+// 			<li>
+// 				You can use your keyboard or a gamepad. The ability to remap the
+// 				controls is coming.
+// 			</li>
+// 		</>
+// 	);
+// }
 
 function EarlyPreviewModal({
 	isOpen,
 	onRequestClose,
 	mode,
 }: EarlyPreviewModalProps) {
-	const HeadsUp = mode === 'editor' ? EditorHeadsUp : PlayerHeadsUp;
+	// const HeadsUp = mode === 'editor' ? EditorHeadsUp : PlayerHeadsUp;
 	return (
 		<Modal
 			isOpen={isOpen}
-			title="Thanks for trying out Jump.Club!"
+			title="Thanks for trying out Smaghetti!"
 			onRequestClose={onRequestClose}
 			onOkClick={onRequestClose}
 		>
 			<div className="w-full max-w-2xl">
-				<p className="text-center">
-					We are just getting started and the editor has a <i>looooong</i> ways
-					to go.
+				<p>
+					Smaghetti is just getting started and has a long ways to go. But
+					ultimately it will be a very good level editor for Super Mario Advance
+					4, including the ability upload levels and share them with other
+					people. Stay tuned!
 				</p>
-				<h2>Heads up...</h2>
-				<ul className="p-x 4 my-4 space-y-2">
-					<HeadsUp />
-				</ul>
-				<div className="text-center font-bold">
-					Check out the <InternalLink to="/about">about page</InternalLink>, or
-					the <InternalLink to="/faq">FAQ</InternalLink>, for more details
-				</div>
 			</div>
 		</Modal>
 	);
--- src/components/EarlyPreviewStarburst/EarlyPreviewStarburst.stories.tsx
+++ src/components/EarlyPreviewStarburst/EarlyPreviewStarburst.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Meta } from '@storybook/react';
 
 import { EarlyPreviewStarburst } from './EarlyPreviewStarburst';
+import { EarlyPreviewModal } from './EarlyPreviewModal';
 
 const meta: Meta = {
 	title: 'EarlyPreviewStarburst',
@@ -12,4 +13,12 @@ export default meta;
 
 export const EditorMode = () => {
 	return <EarlyPreviewStarburst mode="editor" />;
+};
+
+export const EditorModeModal = () => {
+	return <EarlyPreviewModal isOpen={true} mode="editor" />;
+};
+
+export const PlayerModeModal = () => {
+	return <EarlyPreviewModal isOpen={true} mode="player" />;
 };
