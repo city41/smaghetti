519a7a44c7fee8f66d43c887ef56eb7f8b9bf0fc
Matt Greer <matt.e.greer@gmail.com>
2021-03-20 10:16:18 -0400

    MuteButton: set at mount, store to localStorage

--- src/components/make/Editor/Toolbox/MuteButton.tsx
+++ src/components/make/Editor/Toolbox/MuteButton.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FaVolumeDown, FaVolumeMute } from 'react-icons/fa';
 
@@ -8,10 +8,27 @@ type MuteButtonProps = {
 	className?: string;
 };
 
+const LOCALSTORAGE_KEY = 'smaghetti_mute';
+
 function MuteButton({ className }: MuteButtonProps) {
 	const [isMuted, setMuted] = useState(
 		process.env.NODE_ENV === 'development' ? true : false
 	);
+
+	useEffect(() => {
+		const lsvalue = localStorage.getItem(LOCALSTORAGE_KEY);
+
+		if (lsvalue === 'true') {
+			setMuted(true);
+		} else if (lsvalue === 'false') {
+			setMuted(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		window._gba.audio.masterVolume = isMuted ? 0 : 1;
+		localStorage.setItem(LOCALSTORAGE_KEY, isMuted.toString());
+	}, [isMuted]);
 
 	const icon = isMuted ? FaVolumeMute : FaVolumeDown;
 
@@ -25,7 +42,6 @@ function MuteButton({ className }: MuteButtonProps) {
 			toggled={isMuted}
 			onClick={() => {
 				setMuted(!isMuted);
-				window._gba.audio.masterVolume = !isMuted ? 0 : 1;
 			}}
 		/>
 	);
