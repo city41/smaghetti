6be31fd3696fd59eeb0c840d247a8ca93efa1bd3
Matt Greer <matt.e.greer@gmail.com>
2021-03-21 17:12:13 -0400

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,32 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png?v=2020"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png?v=2020"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png?v=2020"
+					/>
+					<link rel="manifest" href="/site.webmanifest?v=2020" />
+					<link
+						rel="mask-icon"
+						href="/safari-pinned-tab.svg?v=2020"
+						color="#5bbad5"
+					/>
+					<link rel="shortcut icon" href="/favicon.ico?v=2020" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#ffffff" />
 					<script type="text/javascript" src="/gbajs/util.js"></script>
 					<script type="text/javascript" src="/gbajs/core.js"></script>
 					<script type="text/javascript" src="/gbajs/arm.js"></script>
