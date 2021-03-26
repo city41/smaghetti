4a09821e594d4864b512f9cf9f42ad9fcca6ce31
Matt Greer <matt.e.greer@gmail.com>
2021-03-26 15:06:42 -0400

    render-level: click to show raw

--- src/components/render-level/RenderLevelPage/LevelObject.tsx
+++ src/components/render-level/RenderLevelPage/LevelObject.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { LevelObject as LevelObjectType } from '../../../tiles/parseObjectsFromLevelFile';
 import { TILE_SIZE as ACTUAL_TILE_SIZE } from '../../../tiles/constants';
@@ -12,6 +12,7 @@ const SCALE = 2;
 const TILE_SIZE = ACTUAL_TILE_SIZE * SCALE;
 
 function LevelObject({ object }: LevelObjectProps) {
+	const [showRaw, setShowRaw] = useState(false);
 	const tileType = objectIdToTileType[object.id];
 
 	const x = object.x * TILE_SIZE;
@@ -29,7 +30,14 @@ function LevelObject({ object }: LevelObjectProps) {
 				'bg-gray-300': !tileType,
 			})}
 			style={style}
-		/>
+			onClick={() => setShowRaw((sr) => !sr)}
+		>
+			{showRaw && (
+				<div className="absolute left-0 -bottom-8 h-8 w-52 bg-gray-200 text-gray-900">
+					{object.rawBytes.map((b) => `0x${b.toString(16)}`).join(', ')}
+				</div>
+			)}
+		</div>
 	);
 }
 
