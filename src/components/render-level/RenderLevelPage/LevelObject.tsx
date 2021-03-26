6080103e15dd8095be04771c063b2e9b3d40ad61
Matt Greer <matt.e.greer@gmail.com>
2021-03-26 15:28:04 -0400

    render-level: raw byte descriptions

--- src/components/render-level/RenderLevelPage/LevelObject.tsx
+++ src/components/render-level/RenderLevelPage/LevelObject.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { LevelObject as LevelObjectType } from '../../../tiles/parseObjectsFromLevelFile';
 import { TILE_SIZE as ACTUAL_TILE_SIZE } from '../../../tiles/constants';
 import { objectIdToTileType } from '../../../entities/objectIdMap';
+import { raw } from '@storybook/react';
 
 type LevelObjectProps = {
 	object: LevelObjectType;
@@ -10,6 +11,36 @@ type LevelObjectProps = {
 
 const SCALE = 2;
 const TILE_SIZE = ACTUAL_TILE_SIZE * SCALE;
+
+const rawBytesDesc = {
+	0: 'b/w',
+	1: 'y',
+	2: 'x',
+	3: 'id',
+	4: 'h',
+};
+
+function Raw({ bytes }: { bytes: number[] }) {
+	const bank = bytes[0] >> 6;
+	return (
+		<div
+			className={clsx(
+				'absolute z-10 left-0 -bottom-14 w-52 bg-gray-200 text-gray-900 grid',
+				{
+					'grid-cols-4': bank === 0,
+					'grid-cols-5': bank === 1,
+				}
+			)}
+		>
+			{bytes.map((b, i) => (
+				<div key={i} className="border border-black">
+					<div>0x{b.toString(16)}</div>
+					<div>{rawBytesDesc[i as keyof typeof rawBytesDesc]}</div>
+				</div>
+			))}
+		</div>
+	);
+}
 
 function LevelObject({ object }: LevelObjectProps) {
 	const [showRaw, setShowRaw] = useState(false);
@@ -32,11 +63,7 @@ function LevelObject({ object }: LevelObjectProps) {
 			style={style}
 			onClick={() => setShowRaw((sr) => !sr)}
 		>
-			{showRaw && (
-				<div className="absolute left-0 -bottom-8 h-8 w-52 bg-gray-200 text-gray-900">
-					{object.rawBytes.map((b) => `0x${b.toString(16)}`).join(', ')}
-				</div>
-			)}
+			{showRaw && <Raw bytes={object.rawBytes} />}
 		</div>
 	);
 }
