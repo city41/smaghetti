7f6766990ae599c4c2056bb1fc8f4d33a8d5cd89
Matt Greer <matt.e.greer@gmail.com>
2021-04-14 08:41:21 -0400

    injectLevelIntoSave: sanity check test

---
+++ src/levelData/injectLevelIntoSave.test.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import { injectLevelIntoSave } from './injectLevelIntoSave';
+
+describe('injectLevelIntoSave', () => {
+	function load(path: string): Uint8Array {
+		const buffer = fs.readFileSync(path);
+		return new Uint8Array(buffer);
+	}
+
+	it('should create the same binary file as sma4savtool', () => {
+		const savToolFile = load('./src/levelData/sma4savtool_classic1_2.sav');
+		const levelFile = load('./src/levelData/star_02.level');
+		const emptySaveFile = load('./public/empty.sav');
+
+		const saveFileWithInjectedLevel = injectLevelIntoSave(
+			emptySaveFile,
+			levelFile
+		);
+
+		fs.writeFileSync(
+			'./src/levelData/smaghetti_classic1_2.sav',
+			saveFileWithInjectedLevel
+		);
+
+		expect(Array.from(saveFileWithInjectedLevel)).toEqual(
+			Array.from(savToolFile)
+		);
+	});
+});
