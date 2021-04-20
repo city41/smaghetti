3f1faf52b4884ecb0fdf1a74ec551d89d8c2dc93
Matt Greer <matt.e.greer@gmail.com>
2021-04-20 19:46:36 -0400

    GiantGoomba

---
+++ src/entities/GiantGoomba.ts
@@ -0,0 +1,43 @@
+import type { Entity } from './types';
+import { simpleSpriteBinary } from './util';
+
+/**
+ * not compatible with any existing room types :(
+ */
+const GiantGoomba: Entity = {
+	type: 'GiantGoomba',
+	editorType: 'entity',
+	gameType: 'sprite',
+	dimensions: 'none',
+	romOffset: 0x16ea40,
+	objectId: 0x7c,
+	palette: [
+		0x7f96,
+		0x7fff,
+		0x18c6,
+		0x101a,
+		0x10bf,
+		0x125f,
+		0x25fd,
+		0x369e,
+		0x475f,
+		0x139f,
+		0x177,
+		0x21c,
+		0x29f,
+		0x47bf,
+		0x137f,
+		0x25f,
+	],
+	tiles: [
+		[832, 833, 834, 835],
+		[848, 849, 850, 851],
+		[864, 865, 866, 867],
+	],
+
+	toBinary(x, y) {
+		return simpleSpriteBinary(x, y, this.objectId!);
+	},
+};
+
+export { GiantGoomba };
