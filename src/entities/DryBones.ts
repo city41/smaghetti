11de9254d65a304b57d6bf28441f4c62919aa016
Matt Greer <matt.e.greer@gmail.com>
2021-04-20 21:11:59 -0400

    DryBones

---
+++ src/entities/DryBones.ts
@@ -0,0 +1,44 @@
+import type { Entity } from './types';
+import { simpleSpriteBinary } from './util';
+
+/**
+ * not compatible with any existing room types :(
+ */
+const DryBones: Entity = {
+	type: 'DryBones',
+	editorType: 'entity',
+	gameType: 'sprite',
+	dimensions: 'none',
+	romOffset: 0x167674,
+	objectId: 0x3f,
+	palette: [
+		0x7f96,
+		0x7fff,
+		0x18c6,
+		0x39ce,
+		0x4a52,
+		0x5ef7,
+		0x7a8b,
+		0x7f6e,
+		0x7fd6,
+		0x6f7b,
+		0x19f8,
+		0x2e5c,
+		0x42ff,
+		0x1b1f,
+		0x1a1f,
+		0x1d,
+	],
+	tiles: [
+		[192, 193],
+		[208, 209],
+		[194, 195],
+		[210, 211],
+	],
+
+	toBinary(x, y) {
+		return simpleSpriteBinary(x, y, this.objectId!);
+	},
+};
+
+export { DryBones };
