002b33c9240734d393550c2d043a3ae223cfefea
Matt Greer <matt.e.greer@gmail.com>
2021-05-04 22:49:49 -0400

    GreenParaTroopa: note about 0x80 being alternate version

--- src/entities/GreenParaTroopa.tsx
+++ src/entities/GreenParaTroopa.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 const graphicSetValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+// TODO: 0x80 is a green para troop that flies back and forth
 const GreenParaTroopa: Entity = {
 	paletteCategory: 'enemy',
 	paletteInfo: {
