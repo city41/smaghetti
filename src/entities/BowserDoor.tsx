ca8bed963925ea42b8ebbe462b55a07931b3c05e
Matt Greer <matt.e.greer@gmail.com>
2021-05-26 16:38:00 -0400

    BowserDoor: to decoration

--- src/entities/BowserDoor.tsx
+++ src/entities/BowserDoor.tsx
@@ -1,31 +1,18 @@
+import React from 'react';
 import type { Entity } from './types';
 import { TILE_SIZE } from '../tiles/constants';
-import React from 'react';
 import { TileSpace } from './TileSpace';
-import { TransportSource } from '../components/Transport/TransportSource';
-import { TransportEditDetails } from '../components/details/TransportEditDetails';
 import { encodeObjectSets } from './util';
 import { ANY_SPRITE_GRAPHIC_SET } from './constants';
 
-const DOOR_LOCK_OBJECT_ID = 0xce;
-
-/**
- * so far transports don't work on this door.
- * One problem is the transport's x position is always
- * half a tile off from where the door actually is. possibly
- * some of the unknown bytes in transports account for that,
- * or possibly this isn't really the bowser door at all just
- * a random object that happened to use bowser door tiles
- */
 const BowserDoor: Entity = {
+	paletteCategory: 'decoration',
 	paletteInfo: {
 		title: 'Bowser Door',
-		warning:
-			"Can't figure out how to get this door to work. Possibly it's just for decoration and can't be used to go to another room?",
+		description: 'This can only be used as decoration and not a real door :(',
 	},
 
 	objectSets: encodeObjectSets([[2, 2]]),
-	// this is for the lock sprite, which truly is universal
 	spriteGraphicSets: ANY_SPRITE_GRAPHIC_SET,
 	layer: 'stage',
 	editorType: 'entity',
@@ -62,36 +49,8 @@ const BowserDoor: Entity = {
 		],
 	},
 
-	getTransports(room, x, y, settings) {
-		const dest = settings.destination;
-
-		if (dest) {
-			return [
-				{
-					destRoom: dest.room as number,
-					destX: dest.x as number,
-					destY: dest.y as number,
-					x,
-					y,
-					room,
-					exitType: 0,
-				},
-			];
-		}
-
-		return [];
-	},
-
 	toObjectBinary(x, y) {
 		return [0x40, y, x, this.objectId];
-	},
-
-	toSpriteBinary(x, y, _w, _h, settings) {
-		if (settings.locked) {
-			return [0, DOOR_LOCK_OBJECT_ID, x + 1, y];
-		} else {
-			return [];
-		}
 	},
 
 	simpleRender(size) {
@@ -105,59 +64,17 @@ const BowserDoor: Entity = {
 		);
 	},
 
-	render(showDetails, settings, onSettingsChange) {
+	render() {
 		const style = {
 			width: TILE_SIZE * 2,
 			height: TILE_SIZE * 2,
 		};
 
-		const body = (
-			<div
-				className="relative BowserDoor-bg bg-cover bg-no-repeat"
-				style={style}
-			>
+		return (
+			<div className="BowserDoor-bg bg-cover bg-no-repeat" style={style}>
 				<TileSpace />
-				{settings.locked && (
-					<div
-						className="DoorLock-bg absolute left-3 top-3"
-						style={{ width: TILE_SIZE / 2, height: TILE_SIZE }}
-					/>
-				)}
-				{settings.destination && (
-					<div
-						className="absolute top-0 left-0"
-						style={{ paddingLeft: TILE_SIZE / 2 }}
-					>
-						<TransportSource
-							destRoom={settings.destination.room}
-							destX={settings.destination.x}
-							destY={settings.destination.y}
-							exitType={0}
-						/>
-					</div>
-				)}
 			</div>
 		);
-
-		if (showDetails) {
-			return (
-				<TransportEditDetails
-					width={TILE_SIZE * 2}
-					height={TILE_SIZE * 2}
-					onDestinationSet={(newDestination) => {
-						onSettingsChange({ ...settings, destination: newDestination });
-					}}
-					locked={!!settings.locked}
-					onLockChange={(locked) => {
-						onSettingsChange({ ...settings, locked });
-					}}
-				>
-					{body}
-				</TransportEditDetails>
-			);
-		} else {
-			return body;
-		}
 	},
 };
 
