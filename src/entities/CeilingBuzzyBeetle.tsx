0a0df7c66e30bc04a20501c64320bd4127ecc68e
Matt Greer <matt.e.greer@gmail.com>
2021-06-11 22:35:08 -0400

    CeilingBuzzy/Spiny - remove unused resources

--- src/entities/CeilingBuzzyBeetle.tsx
+++ src/entities/CeilingBuzzyBeetle.tsx
@@ -27,34 +27,6 @@ const CeilingBuzzyBeetle: Entity = {
 	editorType: 'entity',
 	dimensions: 'none',
 
-	resource: {
-		palettes: [
-			[
-				0x7f96,
-				0x7fff,
-				0x18c6,
-				0x26b,
-				0x1b10,
-				0x13b4,
-				0x25fd,
-				0x369e,
-				0x475f,
-				0x1abf,
-				0x1c,
-				0x253f,
-				0x463f,
-				0x7ad1,
-				0x6e2c,
-				0x59a6,
-			],
-		],
-		romOffset: 0x163768,
-		tiles: [
-			[714, 715],
-			[728, 729],
-		],
-	},
-
 	toSpriteBinary(x, y) {
 		return [0, this.objectId, x, y];
 	},
