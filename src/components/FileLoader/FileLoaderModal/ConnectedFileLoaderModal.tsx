4b024dede7f8234b56a40b85776969f1e5e9ba2c
Matt Greer <matt.e.greer@gmail.com>
2021-06-18 10:13:28 -0400

    hmmm, need file loader modal in dev now...

--- src/components/FileLoader/FileLoaderModal/ConnectedFileLoaderModal.tsx
+++ src/components/FileLoader/FileLoaderModal/ConnectedFileLoaderModal.tsx
@@ -48,7 +48,7 @@ function ConnectedFileLoaderModal({ isOpen }: ConnectedFileLoaderModalProps) {
 
 	return (
 		<FileLoaderModal
-			isOpen={isOpen && process.env.NODE_ENV === 'production'}
+			isOpen={isOpen}
 			extractionState={overallExtractionState}
 			romFileState={romFileState}
 			otherFilesState={otherFilesState}
