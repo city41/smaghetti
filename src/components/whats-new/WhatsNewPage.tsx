00cd68d7d9d275fd6e3c9a75a1682f1cb0e38c6d
Matt Greer <matt.e.greer@gmail.com>
2021-07-22 08:23:09 -0400

    FlyingCheepCheepGenerator

--- src/components/whats-new/WhatsNewPage.tsx
+++ src/components/whats-new/WhatsNewPage.tsx
@@ -81,6 +81,11 @@ function WhatsNewPage() {
 		>
 			<div className="max-w-2xl mx-auto pt-16">
 				<h1 className="font-bold text-5xl text-center mb-8">What&apos;s new</h1>
+				<NewEntry title="New Entities" date="2021-07-22">
+					<ul>
+						<li>Flying Cheep Cheep Generator</li>
+					</ul>
+				</NewEntry>
 				<NewEntry title="New Entities" date="2021-07-16">
 					<ul>
 						<li>Bullet Bill Generator - Cardinal</li>
--- src/entities/FlyingCheepCheepGenerator.tsx
+++ src/entities/FlyingCheepCheepGenerator.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import type { Entity } from './types';
 import { TILE_SIZE } from '../tiles/constants';
 import { ANY_BELOW_0x16, ANY_OBJECT_SET } from './constants';
+import { GeneratorFrame } from './components/GeneratorFrame';
 
 const graphicSetValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 const FlyingCheepCheepGenerator: Entity = {
-	// paletteCategory: 'enemy',
+	paletteCategory: 'enemy',
 	paletteInfo: {
-		subCategory: 'enemy-water',
-		title: 'Cheep Cheep - Flying Generator',
-		warning:
-			'Once added to a level, the entire level has flying cheep cheeps throughout. There is probably a second sprite to turn them off?',
+		subCategory: 'enemy-generator',
+		title: 'Flying Cheep Cheep Generator',
+		description:
+			'Once added to a level, there will be flying cheep cheeps jumping everywhere.',
 	},
 
 	objectSets: ANY_OBJECT_SET,
@@ -26,40 +27,19 @@ const FlyingCheepCheepGenerator: Entity = {
 	},
 
 	simpleRender(size) {
+		const style = {
+			backgroundPositionX: '20%',
+			backgroundPositionY: '25%',
+			backgroundSize: '60%',
+		};
+
 		return (
-			<div
-				className="relative RedCheepCheep-bg bg-cover"
-				style={{ width: size, height: size }}
-			>
-				<div className="absolute -bottom-3 left-0 w-full text-center bg-black text-white text-xs">
-					fly-gen
-				</div>
-			</div>
+			<GeneratorFrame size={size} className="RedCheepCheep-bg" style={style} />
 		);
 	},
 
 	render() {
-		const width = TILE_SIZE;
-		const height = TILE_SIZE;
-
-		const labelStyle = {
-			fontSize: 2,
-			bottom: 0,
-		};
-
-		return (
-			<div
-				className="relative RedCheepCheep-bg bg-cover"
-				style={{ width, height }}
-			>
-				<div
-					className="absolute left-0 w-full text-center bg-black text-white"
-					style={labelStyle}
-				>
-					fly-gen
-				</div>
-			</div>
-		);
+		return this.simpleRender(TILE_SIZE);
 	},
 };
 
