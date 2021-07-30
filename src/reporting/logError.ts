095d0db3616595386c044ee38dadf93873e079ff
Matt Greer <matt.e.greer@gmail.com>
2021-07-30 08:32:07 -0400

    logError: send to google as well as a backup

--- src/reporting/logError.ts
+++ src/reporting/logError.ts
@@ -8,6 +8,44 @@ type LogErrorProps = {
 	level_data?: string | null;
 };
 
+const GOOGLE_POST_URL =
+	'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdxD1WFB8tIs0OapMpie1TmfyBToXUuphLhnTtFhOGaMU9S8w/formResponse';
+const GOOGLE_ENTRY_NAMES = {
+	message: 'entry.1360148685',
+	stack: 'entry.283058055',
+	context: 'entry.1698516925',
+	level_data: 'entry.1247355775',
+};
+
+async function sendToGoogle({
+	context,
+	message,
+	stack = null,
+	level_data = null,
+}: LogErrorProps) {
+	const googleData = {
+		[GOOGLE_ENTRY_NAMES.message]: message,
+		[GOOGLE_ENTRY_NAMES.context]: context,
+		[GOOGLE_ENTRY_NAMES.stack]: stack ?? '',
+		[GOOGLE_ENTRY_NAMES.level_data]: level_data ?? '',
+	};
+	const encodedValues = Object.keys(googleData).map((key) => {
+		const encKey = encodeURIComponent(key);
+		const encValue = encodeURIComponent(googleData[key]);
+		return `${encKey}=${encValue}`;
+	});
+	const body = encodedValues.join('&').replace(/%20/g, '+');
+
+	await fetch(GOOGLE_POST_URL, {
+		method: 'POST',
+		mode: 'no-cors',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body,
+	});
+}
+
 async function logError({
 	context,
 	message,
@@ -20,8 +58,11 @@ async function logError({
 	try {
 		await insertError({ user_id, context, message, stack, level_data, url });
 	} catch (e) {
-		console.error('insertError, error:', e);
+		console.error('supabase insertError, error:', e);
 	}
+
+	// also send to google as a backup
+	await sendToGoogle({ message, context, stack, level_data });
 }
 
 export { logError };
