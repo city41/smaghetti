78dbbb4572a2728da611565a565aef0e4e4fa7b2
Matt Greer <matt.e.greer@gmail.com>
2021-08-03 19:31:04 -0400

    WhatsNewPage: note level save fix

--- src/components/whats-new/WhatsNewPage.tsx
+++ src/components/whats-new/WhatsNewPage.tsx
@@ -88,6 +88,12 @@ function WhatsNewPage() {
 		>
 			<div className="max-w-2xl mx-auto pt-16">
 				<h1 className="font-bold text-5xl text-center mb-8">What&apos;s new</h1>
+				<NewEntry title="Level save bug fix" date="2021-08-03">
+					<p>
+						There was a bug preventing levels that had Stone Supports in them
+						from saving. That has now been fixed.
+					</p>
+				</NewEntry>
 				<NewEntry title="New Entities" date="2021-08-03">
 					<ul>
 						<li>Spiny Cheep Cheep Generator</li>
