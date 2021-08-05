c891abcc1da5d468b2a45416c27f09f87c70b380
Matt Greer <matt.e.greer@gmail.com>
2021-08-05 09:02:00 -0400

    PlatformWoodUpDown: two ranges

--- src/entities/PlatformWoodUpDown.tsx
+++ src/entities/PlatformWoodUpDown.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import type { Entity } from './types';
 import { TILE_SIZE } from '../tiles/constants';
 import { ANY_OBJECT_SET } from './constants';
+import { FaHammer } from 'react-icons/fa';
 
-const TRAVEL_RANGE_IN_TILES = 6;
+const ranges = [4, 6] as const;
+type Range = typeof ranges[number];
+
+const travelRangeToObjectId: Record<Range, number> = {
+	4: 0x38,
+	6: 0x28,
+};
 
 const PlatformWoodUpDown: Entity = {
 	paletteCategory: 'gizmo',
 	paletteInfo: {
 		subCategory: 'gizmo-platform',
 		title: 'Platform - Wood Up/Down',
-		description: 'Just goes up and down, not configurable',
+		description: 'Goes up and down, two possible ranges',
 	},
 
 	objectSets: ANY_OBJECT_SET,
@@ -18,12 +25,17 @@ const PlatformWoodUpDown: Entity = {
 	layer: 'actor',
 	editorType: 'entity',
 	dimensions: 'none',
-	objectId: 0x28,
+	objectId: 0x38,
+	alternateObjectIds: Object.values(travelRangeToObjectId),
 	width: 3,
 	height: 1,
+	settingsType: 'single',
+	defaultSettings: { range: 6 },
 
-	toSpriteBinary(x, y) {
-		return [0, 0x28, x, y];
+	toSpriteBinary(x, y, _w, _h, settings) {
+		const range = (settings.range ?? this.defaultSettings!.range) as Range;
+		const objectId = travelRangeToObjectId[range];
+		return [0, objectId, x, y];
 	},
 
 	simpleRender(size) {
@@ -41,7 +53,10 @@ const PlatformWoodUpDown: Entity = {
 		);
 	},
 
-	render() {
+	render(_showDetails, settings, onSettingsChange, entity) {
+		const range = (settings.range ?? this.defaultSettings!.range) as Range;
+
+		const style = { width: TILE_SIZE * 3, height: TILE_SIZE };
 		const platformStyle = { width: TILE_SIZE * 3, height: TILE_SIZE };
 		const cellStyle = { width: TILE_SIZE, height: TILE_SIZE };
 
@@ -54,11 +69,11 @@ const PlatformWoodUpDown: Entity = {
 		);
 
 		const rangeStyle = {
-			height: TILE_SIZE * TRAVEL_RANGE_IN_TILES,
-			top: -TILE_SIZE * TRAVEL_RANGE_IN_TILES,
+			height: TILE_SIZE * range,
+			top: -TILE_SIZE * range,
 		};
 
-		const range = (
+		const rangeBox = (
 			<div
 				className="absolute w-full opacity-20 bg-green-500 pointer-events-none"
 				style={rangeStyle}
@@ -68,9 +83,31 @@ const PlatformWoodUpDown: Entity = {
 		);
 
 		return (
-			<div className="relative">
+			<div className="relative" style={style}>
 				{platform}
-				{range}
+				{rangeBox}
+				{entity && (
+					<div className="absolute top-0 left-0 w-full h-full flex flex-row justify-center align-center gap-x-0.5 z-10">
+						<button
+							onMouseDown={(e) => {
+								e.preventDefault();
+								e.stopPropagation();
+
+								const rangeIndex = ranges.indexOf(range);
+								const newRangeIndex = (rangeIndex + 1) % ranges.length;
+								const newRange = ranges[newRangeIndex];
+								onSettingsChange({
+									range: newRange,
+								});
+							}}
+						>
+							<FaHammer
+								style={{ borderRadius: '10%', padding: 0.5 }}
+								className="w-1.5 h-1.5 bg-gray-700 hover:bg-gray-600 text-white"
+							/>
+						</button>
+					</div>
+				)}
 			</div>
 		);
 	},
