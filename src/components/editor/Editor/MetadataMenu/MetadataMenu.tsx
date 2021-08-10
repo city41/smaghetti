fc708740d95c714e1b3d8bcf4ac59193d12b1eee
Matt Greer <matt.e.greer@gmail.com>
2021-08-10 08:49:35 -0400

    MetadataMenu: use invisible to maintain height in both modes

--- src/components/editor/Editor/MetadataMenu/MetadataMenu.tsx
+++ src/components/editor/Editor/MetadataMenu/MetadataMenu.tsx
@@ -67,51 +67,48 @@ function MetadataMenu({
 		[currentRoomIndex, roomCount, disabled, isManagingLevel]
 	);
 
-	let body;
-	if (isManagingLevel) {
-		body = (
-			<div className="grid place-items-center w-full">
-				<Button onClick={onManageLevelClick}>return to editing</Button>
-			</div>
-		);
-	} else {
-		body = (
-			<>
-				<div className="flex flex-col space-y-2">
-					<div className="w-40 truncate">{levelName}</div>
-					<div className="flex flex-row space-x-2 items-center">
-						<div className="text-gray-500 text-xs">room</div>
-						{[0, 1, 2, 3].map((ri) => {
-							return (
-								<RoomButton
-									key={ri}
-									index={ri + 1}
-									isCurrent={ri === currentRoomIndex}
-									disabled={!!disabled || ri >= roomCount}
-									onClick={() => onRoomIndexChange(ri)}
-								/>
-							);
-						})}
-					</div>
-				</div>
-				<PlainIconButton
-					icon={MdEdit}
-					label="manage level"
-					onClick={onManageLevelClick}
-					disabled={disabled}
-				/>
-			</>
-		);
-	}
-
 	return (
 		<div
 			className={clsx(
 				className,
-				'py-2 px-4 bg-gray-900 flex flex-row items-center justify-between pointer-events-auto'
+				'relative py-2 px-4 bg-gray-900 flex flex-row items-center justify-between pointer-events-auto'
 			)}
 		>
-			{body}
+			<div
+				className={clsx('absolute grid place-items-center h-full w-full', {
+					invisible: !isManagingLevel,
+				})}
+			>
+				<Button onClick={onManageLevelClick}>return to editing</Button>
+			</div>
+			<div
+				className={clsx('flex flex-col space-y-2', {
+					invisible: isManagingLevel,
+				})}
+			>
+				<div className="w-40 truncate">{levelName}</div>
+				<div className="flex flex-row space-x-2 items-center">
+					<div className="text-gray-500 text-xs">room</div>
+					{[0, 1, 2, 3].map((ri) => {
+						return (
+							<RoomButton
+								key={ri}
+								index={ri + 1}
+								isCurrent={ri === currentRoomIndex}
+								disabled={!!disabled || ri >= roomCount}
+								onClick={() => onRoomIndexChange(ri)}
+							/>
+						);
+					})}
+				</div>
+			</div>
+			<PlainIconButton
+				className={clsx({ invisible: isManagingLevel })}
+				icon={MdEdit}
+				label="manage level"
+				onClick={onManageLevelClick}
+				disabled={disabled}
+			/>
 		</div>
 	);
 }
