e9510de743795e3311cd77c6e096f26870a7c9ec
Matt Greer <matt.e.greer@gmail.com>
2021-08-16 09:05:42 -0400

    getPublishedLevels: only select first room

--- src/remoteData/getPublishedLevels.ts
+++ src/remoteData/getPublishedLevels.ts
@@ -1,9 +1,18 @@
 import { client } from './client';
 
+// this function is weird. Supabase's postrest client can do json queries, but it seems like grabbing
+// the first room this way data->rooms[0] is a hack. It works, but the resulting data comes back in
+// a strange shape that needs to be molded into a SerializedLevel.
+//
+// also, data->rooms[0] must be the last thing in the query string, everything after it is ignored
+
 async function getPublishedLevels(): Promise<SerializedLevel[]> {
 	const { data, error } = await client
-		.from<SerializedLevel>('levels')
-		.select('*, user:user_id (username)')
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		.from<any>('levels')
+		.select(
+			'id, name, created_at, updated_at, version, data->settings, user:user_id (username), data->rooms[0]'
+		)
 		.eq('published', true)
 		.order('updated_at', { ascending: false })
 		.limit(40);
@@ -12,7 +21,12 @@ async function getPublishedLevels(): Promise<SerializedLevel[]> {
 		throw error;
 	}
 
-	return data ?? [];
+	return (data ?? []).map((row) => {
+		return {
+			...row,
+			data: { rooms: row.rooms, settings: row.settings },
+		};
+	}) as SerializedLevel[];
 }
 
 export { getPublishedLevels };
