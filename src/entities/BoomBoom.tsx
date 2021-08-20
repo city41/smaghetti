d83e98e6fee8e4da602868fa478687d1e665ea88
Matt Greer <matt.e.greer@gmail.com>
2021-08-19 23:40:04 -0400

    BoomBoom: remove warning that no longer applies

--- src/entities/BoomBoom.tsx
+++ src/entities/BoomBoom.tsx
@@ -11,8 +11,6 @@ const BoomBoom: Entity = {
 	paletteInfo: {
 		subCategory: 'enemy-boss',
 		title: 'Boom Boom',
-		warning:
-			"Sometimes BoomBoom doesn't spawn or spawns in a different place than where you placed him. Not sure why yet...",
 	},
 
 	objectSets: ANY_OBJECT_SET,
