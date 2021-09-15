3111255cca1f6d4b40455858116026f4ccd99268
Matt Greer <matt.e.greer@gmail.com>
2021-09-14 21:24:11 -0400

    Muncher: a lot more object sets

--- src/entities/Muncher/Muncher.tsx
+++ src/entities/Muncher/Muncher.tsx
@@ -1,8 +1,9 @@
-import type { Entity } from './types';
-import { TILE_SIZE } from '../tiles/constants';
+import type { Entity } from '../types';
+import { TILE_SIZE } from '../../tiles/constants';
 import React from 'react';
-import { ANY_SPRITE_GRAPHIC_SET } from './constants';
-import { encodeObjectSets, parseSimpleObject } from './util';
+import { ANY_SPRITE_GRAPHIC_SET } from '../constants';
+import { encodeObjectSets, parseSimpleObject } from '../util';
+import { objectSets } from './objectSets';
 
 const Muncher: Entity = {
 	paletteCategory: 'terrain',
@@ -11,36 +12,7 @@ const Muncher: Entity = {
 		title: 'Muncher',
 	},
 
-	// TODO: munchers came up in many different sets with
-	// different color palettes and such. just chose the biggest one for now
-	objectSets: encodeObjectSets([
-		[1, 10],
-		[1, 11],
-		[1, 12],
-		[1, 13],
-		[1, 14],
-		[1, 1],
-		[1, 2],
-		[1, 3],
-		[1, 4],
-		[1, 5],
-		[1, 6],
-		[1, 8],
-		[1, 9],
-		[11, 10],
-		[11, 11],
-		[11, 12],
-		[11, 13],
-		[11, 14],
-		[11, 1],
-		[11, 2],
-		[11, 3],
-		[11, 4],
-		[11, 5],
-		[11, 6],
-		[11, 8],
-		[11, 9],
-	]),
+	objectSets: encodeObjectSets(objectSets),
 	spriteGraphicSets: ANY_SPRITE_GRAPHIC_SET,
 	layer: 'stage',
 	editorType: 'cell',
