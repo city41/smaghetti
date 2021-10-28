afd8d7c5f29ccec6e1bb5409f7ad50f83575df85
Matt Greer <matt.e.greer@gmail.com>
2021-10-28 15:30:27 -0400

    decent looking keyboard side

--- src/components/ControlsBanner/KeyConfigModal.tsx
+++ src/components/ControlsBanner/KeyConfigModal.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement, ReactNode, useEffect } from 'react';
 
 import { Modal } from '../Modal';
-import { IconGamepad, IconKeyboard } from '../../icons';
+import {
+	IconArrowDown,
+	IconArrowLeft,
+	IconArrowRight,
+	IconArrowUp,
+	IconGamepad,
+	IconKeyboard,
+} from '../../icons';
+import { Button } from '../Button';
 
 type KeyConfigModalProps = {
 	isOpen: boolean;
@@ -10,7 +18,7 @@ type KeyConfigModalProps = {
 
 function Kbd({ children }: { children: ReactNode }) {
 	return (
-		<kbd className="px-2 py-1 bg-gray-500 text-white font-bold rounded-md">
+		<kbd className="inline-block text-center px-2 py-1 bg-gray-500 text-white font-bold rounded-md">
 			{children}
 		</kbd>
 	);
@@ -22,6 +30,21 @@ function GamePadButton({ children }: { children: ReactNode }) {
 			{children}
 		</span>
 	);
+}
+
+function keyCodeToString(keyCode: number): ReactNode {
+	switch (keyCode) {
+		case 37:
+			return <IconArrowLeft />;
+		case 38:
+			return <IconArrowUp />;
+		case 39:
+			return <IconArrowRight />;
+		case 40:
+			return <IconArrowDown />;
+		default:
+			return String.fromCharCode(keyCode);
+	}
 }
 
 function KeyConfigModal({
@@ -59,22 +82,39 @@ function KeyConfigModal({
 						</p>
 					</div>
 				</div>
-				<div className="bg-gray-600 p-4">
+				<div className="bg-gray-600 p-4 flex flex-col gap-y-4 items-center">
 					<IconKeyboard className="text-4xl mx-auto mb-4" />
-					<ul className="space-y-2 text-center">
-						<li>
-							<Kbd>arrow keys</Kbd> - move
-						</li>
-						<li>
-							<Kbd>X</Kbd> - jump
-						</li>
-						<li>
-							<Kbd>Z</Kbd> - run
-						</li>
-					</ul>
-				</div>
-				<div className="mt-4 px-4 py-2 bg-green-500 text-white col-span-2 text-center">
-					Allowing controls to be configured is on the todo list!
+					<div className="flex flex-row gap-x-4 items-center justify-center">
+						<div
+							className="grid gap-x-2 gap-y-2 items-center mx-auto"
+							style={{ gridTemplateColumns: 'repeat(3, min-content)' }}
+						>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_LEFT)}</Kbd>
+							<div>-</div>
+							<div>left</div>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_RIGHT)}</Kbd>
+							<div>-</div>
+							<div>right</div>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_UP)}</Kbd>
+							<div>-</div>
+							<div>up</div>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_DOWN)}</Kbd>
+							<div>-</div>
+							<div>down</div>
+						</div>
+						<div
+							className="grid gap-x-2 gap-y-2 items-center mx-auto"
+							style={{ gridTemplateColumns: 'repeat(3, min-content)' }}
+						>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_A)}</Kbd>
+							<div>-</div>
+							<div>jump</div>
+							<Kbd>{keyCodeToString(window.GBA_KEYCODE_B)}</Kbd>
+							<div>-</div>
+							<div>run</div>
+						</div>
+					</div>
+					<Button>reassign</Button>
 				</div>
 			</div>
 		</Modal>
