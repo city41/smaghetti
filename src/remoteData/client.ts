1466b2d28d0b7f1bb73a9d19ae85578a926cf07f
Matt Greer <matt.e.greer@gmail.com>
2022-01-14 10:50:08 -0500

    remoteData/client: mock client to get editor working without supabase

--- src/remoteData/client.ts
+++ src/remoteData/client.ts
@@ -2,13 +2,22 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 let client: SupabaseClient;
 
+const MOCK_CLIENT = {
+	auth: {
+		user() {
+			return null;
+		},
+		onAuthStateChange() {},
+	},
+};
+
 try {
 	client = createClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL as string,
 		process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 	);
 } catch {
-	client = ({} as unknown) as SupabaseClient;
+	client = (MOCK_CLIENT as unknown) as SupabaseClient;
 }
 
 export { client };
