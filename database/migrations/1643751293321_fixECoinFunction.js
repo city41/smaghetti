3e0d5ebdeed33d607c75f96cecb704c963e46961
Matt Greer <matt.e.greer@gmail.com>
2022-02-01 16:36:18 -0500

    migration: fix ecoin function

---
+++ database/migrations/1643751293321_fixECoinFunction.js
@@ -0,0 +1,53 @@
+const GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_NAME =
+	'get_published_levels_that_have_ecoins';
+const GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_PARAMS = [];
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+	pgm.createFunction(
+		GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_NAME,
+		GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_PARAMS,
+		{
+			replace: true,
+			returns:
+				'table (id text, name text, data jsonb, version text, username text, created_at timestamp, updated_at timestamp, total_vote_count bigint)',
+			language: 'plpgsql security definer',
+		},
+		`
+    begin
+      return query
+      select 
+        l.id,
+        l.name,
+        l.data,
+        l.version,
+        u.username,
+        l.created_at,
+        l.updated_at,
+        coalesce(count(lv.user_id), 0) as total_vote_count
+      from
+        public.levels l
+      left join
+        public.level_votes lv
+      on
+        lv.level_id = l.id 
+      left join
+        public.users u
+      on
+        u.id = l.user_id
+      where
+        l.published = true and (u.role is null or u.role <> 'sb') and (l.data::text like '%"type": "ECoin"%')
+      group by
+        l.id, l.name, l.data, l.version, u.username, l.created_at, l.updated_at;
+      end;
+    `
+	);
+};
+
+exports.down = (pgm) => {
+	pgm.dropFunction(
+		GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_NAME,
+		GET_PUBLISHED_LEVELS_THAT_HAVE_ECOINS_FUNCTION_PARAMS
+	);
+};
