759c3806652c1b60fe9a36a20ac1cfff06112138
Matt Greer <matt.e.greer@gmail.com>
2023-07-17 08:16:59 -0400

    cleared likes

--- scripts/createLevelsArchive.js
+++ scripts/createLevelsArchive.js
@@ -10,7 +10,7 @@ const FETCH_CONFIG = {
 		apikey:
 			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNjU1NTIxMywiZXhwIjoxOTMyMTMxMjEzfQ.oT8a5pEGoqiPFWwuyoe-kVWvhCIn6XTnQZDFDuA2AiE',
 		authorization:
-			'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IktXWld1WmFtYXE0RGtWc2ciLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjkwMDY0MDA4LCJpYXQiOjE2ODk0NTkyMDgsImlzcyI6Imh0dHBzOi8vaHR0cHM6Ly9jZm15cGthc2l4am5wa3hmcHlsZi5zdXBhYmFzZS5jby9hdXRoL3YxIiwic3ViIjoiMzE3ZDg0YTktNTRmYy00MTA5LTg4NDQtYjFjMjExYWIxYzI2IiwiZW1haWwiOiJtYXR0QHNtYWdoZXR0aS5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIn0sInVzZXJfbWV0YWRhdGEiOnt9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJzZXNzaW9uX2lkIjoiMDI2OTQxODQtOWFiYi00MWFkLWFkOWYtOWMxMDIwNWM4MzVhIn0.8nmNHZi478WXXl7qik1T96Z-0UqN4XgDdDOBFDFw7P8',
+			'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNjU1NTIxMywiZXhwIjoxOTMyMTMxMjEzfQ.oT8a5pEGoqiPFWwuyoe-kVWvhCIn6XTnQZDFDuA2AiE',
 		'content-profile': 'public',
 		'content-type': 'application/json',
 		prefer: 'count=exact',
@@ -25,7 +25,7 @@ const FETCH_CONFIG = {
 		Referer: 'https://smaghetti.com/',
 		'Referrer-Policy': 'strict-origin-when-cross-origin',
 	},
-	body: '{"current_user_id":"317d84a9-54fc-4109-8844-b1c211ab1c26"}',
+	body: '{"current_user_id":""}',
 	method: 'POST',
 };
 
